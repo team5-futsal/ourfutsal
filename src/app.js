dd280f378b83bb50aafa8a51938e987d85ac42b7
NterChoi <choi16289@naver.com>
2024-09-15 19:39:48 +0900

    선수 생성, 전체 선수 조회 완료 선수 상세 조회, 선수 능력치 수정 진행중

--- src/app.js
+++ src/app.js
@@ -6,13 +6,14 @@ import charactersRouter from './routes/characters.router.js'
 import itemsRouter from './routes/items.router.js'
 import inventoryRouter from './routes/inventory.router.js'
 import gearsRouter from './routes/gears.router.js'
+import PlayersRouter from './routes/players.router.js'
 
 const app = express();
 const PORT = 3333;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', [UsersRouter, charactersRouter, itemsRouter, inventoryRouter, gearsRouter]);
+app.use('/api', [UsersRouter, charactersRouter, itemsRouter, inventoryRouter, gearsRouter, PlayersRouter]);
 
 // 에러 핸들링 미들웨어를 등록합니다.
 app.use(ErrorHandlerMiddleware);
