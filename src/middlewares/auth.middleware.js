461a37dd5411963dc953cbe78615d5bb06a0f4c7
soonode97 <soonode951@gmail.com>
2024-09-15 21:54:51 +0900

    api 카테고리 html UI 업데이트

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,46 +1,42 @@
 import { prisma } from '../utils/prisma/index.js';
-import {validateToken} from '../utils/tokens/tokens.js';
+import { validateToken } from '../utils/tokens/tokens.js';
 
 export default async function (req, res, next) {
-  try {
-    const authorization = req.headers['authorization'];
-    if (!authorization) throw new Error('토큰이 존재하지 않습니다.');
+    try {
+        const authorization = req.headers['authorization'];
+        if (!authorization) throw new Error('토큰이 존재하지 않습니다.');
 
-    const [tokenType, token] = authorization.split(' ');
+        const [tokenType, token] = authorization.split(' ');
 
-    if (tokenType !== 'Bearer')
-      throw new Error('토큰 타입이 일치하지 않습니다.');
-    
-    const decodedToken = validateToken(token, process.env.OUR_SECRET_ACCESS_KEY);
+        if (tokenType !== 'Bearer') throw new Error('토큰 타입이 일치하지 않습니다.');
 
-    const accountId = decodedToken.accountId;
+        const decodedToken = validateToken(token, process.env.OUR_SECRET_ACCESS_KEY);
 
-    const user = await prisma.account.findFirst({
-      where: { accountId: +accountId },
+        const accountId = decodedToken.accountId;
 
-    });
-    if (!user) {
-      res.clearCookie('authorization');
-      throw new Error('토큰 사용자가 존재하지 않습니다.');
+        const user = await prisma.account.findFirst({
+            where: { accountId: +accountId },
+        });
+        if (!user) {
+            res.clearCookie('authorization');
+            throw new Error('토큰 사용자가 존재하지 않습니다.');
+        }
+
+        // req.user에 사용자 정보를 저장합니다.
+        req.user = user;
+
+        next();
+    } catch (error) {
+        res.clearCookie('authorization');
+
+        // 토큰이 만료되었거나, 조작되었을 때, 에러 메시지를 다르게 출력합니다.
+        switch (error.name) {
+            case 'TokenExpiredError':
+                return res.status(401).json({ message: '토큰이 만료되었습니다.' });
+            case 'JsonWebTokenError':
+                return res.status(401).json({ message: '토큰이 조작되었습니다.' });
+            default:
+                return res.status(401).json({ message: error.message ?? '비정상적인 요청입니다.' });
+        }
     }
-
-    // req.user에 사용자 정보를 저장합니다.
-    req.user = user;
-
-    next();
-  } catch (error) {
-    res.clearCookie('authorization');
-    
-    // 토큰이 만료되었거나, 조작되었을 때, 에러 메시지를 다르게 출력합니다.
-    switch (error.name) {
-      case 'TokenExpiredError':
-        return res.status(401).json({ message: '토큰이 만료되었습니다.' });
-      case 'JsonWebTokenError':
-        return res.status(401).json({ message: '토큰이 조작되었습니다.' });
-      default:
-        return res
-          .status(401)
-          .json({ message: error.message ?? '비정상적인 요청입니다.' });
-    }
-  }
 }
