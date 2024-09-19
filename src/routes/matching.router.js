8ea7d28494b83c858c346b95b1bd98f2591024bc
skybanana <112135946+skybanana@users.noreply.github.com>
2024-09-19 16:52:37 +0900

    feat : utils/errors 에러 분류 기능(미적용)

--- src/routes/matching.router.js
+++ src/routes/matching.router.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { prisma } from '../utils/prisma/index.js';
 import authMiddleware from '../middlewares/auth.middleware.js';
 import validSchema from '../utils/joi/valid.schema.js';
+import errors from '../utils/errors/error.constructor.js'
 
 const router = express.Router();
 
