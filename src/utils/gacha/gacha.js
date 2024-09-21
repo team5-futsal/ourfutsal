ac1047ed8e7c430f30076bf2eaadf906bea309fc
liha-engineer <liha.engineer@gmail.com>
2024-09-22 00:39:56 +0900

    feat: gacha.js 중간저장

--- src/utils/gacha/gacha.js
+++ src/utils/gacha/gacha.js
@@ -7,35 +7,38 @@ export default async function (req, res, next) {
     const findProduct = await prisma.product.findUnique({
         where: { productId: +productId },
     });
-    if(!findProduct)
-        return res.status(404).json({ message : "존재하지 않는 상품입니다. "})
+    if (!findProduct) return res.status(404).json({ message: '존재하지 않는 상품입니다. ' });
+
     const findGacha = await prisma.gacha.findFirst({
         where: { productId: +productId },
     });
-    if(!findGacha)
-        return res.status(404).json({ message : "존재하지 않는 가챠입니다. "})
+    if (!findGacha) return res.status(404).json({ message: '존재하지 않는 가챠입니다. ' });
 
     const playerCount = await prisma.player.count();
-    const randomPlayers = Math.floor(Math.random() * playerCount) + 1;
     const gachaQuantity = findGacha.gachaQuantity;
-    const gachaPrice = findProduct.price * findGacha.gachaQuantity;
+    const gachaPrice = findProduct.price;
 
-    if (req.user.cash < gachaPrice) return res.status(400).json({ message: '소지금이 부족합니다' });
+    if (req.user.cash < gachaPrice)
+        return res.status(400).json({ message: `소지금이 부족합니다. 가격 ${gachaPrice}, 소지금 ${req.user.cash}` });
 
     const gachaLogic = async function (accountId, gachaQuantity) {
         let pickedPlayer = [];
         for (let i = 0; i < gachaQuantity; i++) {
-            randomPlayers;
+            const randomPlayers = Math.floor(Math.random() * playerCount) + 1;
 
-            const getGachaResult = await prisma.roster.create({
+            const getGachaResult = await prisma.roster.createMany({
                 data: {
                     accountId: +accountId,
                     playerId: randomPlayers,
                 },
             });
-            pickedPlayer[i] = getGachaResult;
+
+            const findPlayer = await prisma.player.findFirst({
+                where : {playerId : +randomPlayers}
+            })
+            pickedPlayer.push(findPlayer.playerName);
         }
         return pickedPlayer;
-    }
-    return gachaLogic(accountId, gachaQuantity)
+    };
+    return gachaLogic(accountId, gachaQuantity);
 }
