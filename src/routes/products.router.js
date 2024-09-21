9c2ecf56fc87a265bdab7f10ee2df2e679482521
liha-engineer <liha.engineer@gmail.com>
2024-09-22 00:40:37 +0900

    feat: 가챠 로직 적용여부 수정 중

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -3,6 +3,7 @@ import { prisma } from '../utils/prisma/index.js';
 import authMiddleware from '../middlewares/auth.middleware.js';
 import { Prisma } from '@prisma/client';
 import gacha from '../utils/gacha/gacha.js';
+// import { gachaLogic } from '../utils/gacha/gacha.js'
 
 const router = express.Router();
 // router.use(authMiddleware);
@@ -16,8 +17,7 @@ router.put('/account/:accountId', authMiddleware, async (req, res, next) => {
     const findAccount = await prisma.account.findFirst({
         where: { accountId: +accountId },
     });
-    if(!findAccount)
-        return res.status(401).json({ message : "유효하지 않은 사용자입니다."})
+    if (!findAccount) return res.status(401).json({ message: '유효하지 않은 사용자입니다.' });
     if (cash <= 0) return res.status(400).json({ message: '올바른 금액을 입력해 주세요.' });
     if (cash > Number.MAX_SAFE_INTEGER) return res.status(400).json({ message: '입력 가능한 금액을 초과하였습니다.' });
 
@@ -47,9 +47,8 @@ router.post('/product', async (req, res, next) => {
     return res.status(201).json({ message: '상품 생성 완료', data: makeProduct });
 });
 
-
 const purchaseHistoryList = {
-    purchaseId : true,
+    purchaseId: true,
     productId: true,
     purchaseQuantity: true,
     beforePurchaseCash: true,
@@ -59,9 +58,7 @@ const purchaseHistoryList = {
 
 // 구매이력조회
 router.get('/product/purchasehistory', async (req, res, next) => {
-    const purchaseHistory = await prisma.purchaseHistory.findMany({
-
-    });
+    const purchaseHistory = await prisma.purchaseHistory.findMany({});
     return res.status(200).json({ message: '조회 완료', data: purchaseHistory });
 });
 
@@ -103,35 +100,19 @@ router.post('/product/:productId', authMiddleware, async (req, res, next) => {
     const { count } = req.body;
     const { cash } = req.user;
 
-    //상점 상품 있는지 찾기
+    //상점 상품 있는지 찾기 -> 이때 가챠의 여부까지 다 나와야 한다
     const findProduct = await prisma.product.findUnique({
         where: { productId: +productId },
+        // gacha : {
+
+        // }
     });
     if (!findProduct) return res.status(404).json({ message: '상품이 존재하지 않습니다' });
     if (findProduct.price * count > cash) return res.status(400).json({ message: '소지금이 부족합니다' });
 
-    // 이 부분에서 가챠 로직이 돌아가면 좋겠다 (선수추출 및 선수보관함에 저장)
-    // 가챠 로직은 함수로? -> 웅상님이 작성해주신 라우터가 있다
-    // -> 웅상님 라우터도 별도의 함수를 라우터 안에서 실행시키는 식이다
-    // 상점 상품 구매와 가챠 구매 라우터를 어떻게 처리할까?🤔 묶을 수 있을까? 함수?
-
-    // 이 상품아이디가 가챠상품인지 가챠테이블에서 찾기
-    const findInGacha = await prisma.gacha.findFirst({
-        where: { productId: +productId },
-    });
-
-    // 가챠가 맞으면 함수 실행
-    if (findInGacha) {
-        try {
-            const doGacha = await gacha(req, res, next);
-            return res.status(201).json({ message: `${doGacha} 선수를 획득했습니다!. 남은 재화는 ${cash}` });
-        } catch (e) {
-            next(e);
-        }
-    }
-
-    // 여기는 트랜잭션으로 묶어주자
-    const buyingProductTransaction = prisma.$transaction(
+    // 구매 후 잔액변동 및 구매이력 생성 로직부터 -> 선불!
+    
+    const buyingTransaction = await prisma.$transaction(
         async tx => {
             const changeBalance = await tx.account.update({
                 where: { accountId: +req.user.accountId },
@@ -142,7 +123,7 @@ router.post('/product/:productId', authMiddleware, async (req, res, next) => {
                     productId: +productId,
                     purchaseQuantity: count,
                     beforePurchaseCash: cash,
-                    afterPurchaseCash: cash - findProduct.price * count,
+                    afterPurchaseCash: -findProduct.price * count,
                 },
             });
             return [changeBalance, makePurchaseHistory];
@@ -152,9 +133,23 @@ router.post('/product/:productId', authMiddleware, async (req, res, next) => {
         },
     );
 
+    // 이 상품아이디가 가챠상품인지 가챠테이블에서 찾기
+    const findInGacha = await prisma.gacha.findFirst({
+        where: { productId: +productId },
+    });
+
+    // 가챠가 맞으면 함수 실행
+    if (findInGacha) {
+        try {
+            const doGacha = await gacha(req, res, next);
+            return res.status(201).json({ message: `${doGacha} 선수 획득했습니다.잔액 ${buyingTransaction[0].cash}` });
+        } catch (e) {
+            next(e);
+        }
+    }
+
     return res.status(200).json({
-        message: `${findProduct.productName} 아이템 ${count}개 구매 성공, 
-            잔액 ${req.user.cash}`,
+        message: `${findProduct.productName} 아이템 ${count}개 구매 성공, 잔액 ${buyingTransaction[0].cash}`,
     });
 });
 
