6b3940bbd42f4befcd2ee580807fd831a7c6e410
skybanana <112135946+skybanana@users.noreply.github.com>
2024-09-23 20:13:20 +0900

    fix : canvas position

---
+++ src/browser/scripts/play.js
@@ -0,0 +1,19 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d');
+
+class player{
+    constructor(){
+        this.x = 10;
+        this.y = 200;
+        this.width = 50;
+        this.height = 50;
+    }
+
+    draw(){
+        ctx.fillStyle = 'blue';
+        ctx.fillRect(this.x,this.y,this.width,this.height);
+    }
+}
+
+const dino = new player()
+dino.draw()
