d604ea8ee0c1f304db0ed4dd4203dd7775f45cd6
skybanana <112135946+skybanana@users.noreply.github.com>
2024-09-23 21:38:44 +0900

    feat : canvas test

--- src/browser/scripts/play.js
+++ src/browser/scripts/play.js
@@ -1,25 +1,28 @@
-const canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
+document.addEventListener('DOMContentLoaded', function() {
+    const canvas = document.getElementById('canvas');
+    let ctx = canvas.getContext('2d');
 
-export class player {
-    constructor() {
-        this.x = 10;
-        this.y = 200;
-        this.width = 50;
-        this.height = 50;
+    class player {
+        constructor() {
+            this.x = 10;
+            this.y = 200;
+            this.width = 50;
+            this.height = 50;
+        }
+
+        draw() {
+            ctx.fillStyle = 'blue';
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
 
-    draw() {
-        ctx.fillStyle = 'blue';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-}
-
-const dino = new player()
-dino.draw()
+    const dino = new player()
+    dino.draw()
+});
 
 //https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/lineTo
 //https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/stroke
 //https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
 //https://developer.mozilla.org/ko/docs/Web/API/Window/requestAnimationFrame
-
+//<!-- //Requestanimation frame  https://developer.mozilla.org/ko/docs/Web/API/Window/requestAnimationFrame -->
+//<!-- https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D -->
