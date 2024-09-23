2e6470b72f98b78ba148683e8f913ac2bb2eeaf7
skybanana <112135946+skybanana@users.noreply.github.com>
2024-09-24 00:51:24 +0900

    feat : draw player and ball

--- src/browser/scripts/play.js
+++ src/browser/scripts/play.js
@@ -1,28 +1,66 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const canvas = document.getElementById('canvas');
-    let ctx = canvas.getContext('2d');
+const canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
 
-    class player {
-        constructor() {
-            this.x = 10;
-            this.y = 200;
-            this.width = 50;
-            this.height = 50;
-        }
+const ballImage = document.getElementById("source");
 
-        draw() {
-            ctx.fillStyle = 'blue';
-            ctx.fillRect(this.x, this.y, this.width, this.height);
-        }
+class drawPlayer {
+    constructor(ctx) {
+        this.width = 40;
+        this.height = 40;
+        this.ctx = ctx
     }
 
-    const dino = new player()
-    dino.draw()
-});
+    draw(x,y, radius, color) {
+        ctx.beginPath();
+        this.ctx.arc(x, y, radius, 0, 2 * Math.PI)
+        ctx.stroke();
+        this.ctx.fillStyle = color;
+        this.ctx.fill()
 
+        this.ctx.font = "30px malgun gothic"
+        this.ctx.fillStyle = 'white';
+        this.ctx.fillText("name", x-radius, y, 500)
+    }
+
+    drawSp(x,y,radius){
+        this.ctx.strokeStype = 'rgb(0 0 0)';
+        this.ctx.lineWidth = 10;
+        this.ctx.strokeRect(x, y+radius, radius*2, 10);
+        this.ctx.fillStyle = 'rgb(255 255 0)';
+        this.ctx.fillRect(x, y+radius, radius*2, 10);
+    }
+}
+
+class drawBall {
+    constructor(ctx, image) {
+        this.width = 40;
+        this.height = 40;
+        this.ctx = ctx
+        this.image = image
+    }
+
+    draw(x,y) {
+        this.ctx.drawImage(this.image, x,y,50,50)
+    }
+}
+
+const player = new drawPlayer(ctx)
+const ball = new drawBall(ctx, ballImage)
+
+const heightUnit = canvas.height/3<<0
+const halfHeightUnit = heightUnit/2<<0
+const yPosision = [halfHeightUnit, halfHeightUnit+heightUnit*2, halfHeightUnit+heightUnit]
+const radius = heightUnit/4<<0
+const color = ['blue', 'red']
+
+player.draw(radius, yPosision[2], radius, color[0])
+player.drawSp(0, yPosision[2], radius)
+ball.draw(radius, yPosision[2])
+
+//Reference
 //https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/lineTo
 //https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/stroke
 //https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
 //https://developer.mozilla.org/ko/docs/Web/API/Window/requestAnimationFrame
-//<!-- //Requestanimation frame  https://developer.mozilla.org/ko/docs/Web/API/Window/requestAnimationFrame -->
-//<!-- https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D -->
+//https://developer.mozilla.org/ko/docs/Web/API/Window/requestAnimationFrame
+//https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
