1457430405f4ea1713029b8ee55e5ce99718e798
liha-engineer <liha.engineer@gmail.com>
2024-09-23 13:17:25 +0900

    feat: 가챠 로직 수정 중

--- src/utils/gacha/gacha.js
+++ src/utils/gacha/gacha.js
@@ -1,44 +1,29 @@
 import { prisma } from '../prisma/index.js';
 
-export default async function (req, res, next) {
-    const { productId } = req.params;
-    const { accountId } = req.user;
-
-    const findProduct = await prisma.product.findUnique({
-        where: { productId: +productId },
-    });
-    if (!findProduct) return res.status(404).json({ message: '존재하지 않는 상품입니다. ' });
-
-    const findGacha = await prisma.gacha.findFirst({
-        where: { productId: +productId },
-    });
-    if (!findGacha) return res.status(404).json({ message: '존재하지 않는 가챠입니다. ' });
-
+export default async function (accountId, gachaQuantity) {
     const playerCount = await prisma.player.count();
-    const gachaQuantity = findGacha.gachaQuantity;
-    const gachaPrice = findProduct.price;
+    const allPlayersId = [];
+    const pickedPlayer = [];
 
-    if (req.user.cash < gachaPrice)
-        return res.status(400).json({ message: `소지금이 부족합니다. 가격 ${gachaPrice}, 소지금 ${req.user.cash}` });
+    // 플레이어DB에서 모두 검색 및 릴레이션 데이터까지 추출
+    const findPlayers = await prisma.player.findMany({
+        include: true,
+    });
 
-    const gachaLogic = async function (accountId, gachaQuantity) {
-        let pickedPlayer = [];
-        for (let i = 0; i < gachaQuantity; i++) {
-            const randomPlayers = Math.floor(Math.random() * playerCount) + 1;
+    // 모든 플레이어의 playerId 추출
+    for (let i in findPlayers) {
+        allPlayersId.push(findPlayers[i].playerId);
+    }
 
-            const getGachaResult = await prisma.roster.createMany({
-                data: {
-                    accountId: +accountId,
-                    playerId: randomPlayers,
-                },
-            });
+    for (let i = 0; i < gachaQuantity; i++) {
+        const randomPlayers = Math.floor(Math.random() * allPlayersId.length) + 1;
+        pickedPlayer.push({
+            playerId: allPlayersId[randomPlayers],
+            accountId : +accountId
+        });
+        console.log('randomPlayers는: ', randomPlayers);
+    }
 
-            const findPlayer = await prisma.player.findFirst({
-                where : {playerId : +randomPlayers}
-            })
-            pickedPlayer.push(findPlayer.playerName);
-        }
-        return pickedPlayer;
-    };
-    return gachaLogic(accountId, gachaQuantity);
+
+    return pickedPlayer;
 }
