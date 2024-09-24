5e2b7f24235d4fca2f940db3dca756ed18951e55
liha-engineer <liha.engineer@gmail.com>
2024-09-24 16:42:36 +0900

    feat: 가챠 로직 위치변경 및 ver.1에 트랜잭션 적용

--- src/routes/gacha.router.js
+++ src/routes/gacha.router.js
@@ -26,7 +26,68 @@ router.get('/gacha', async (req, res, next) => {
     return res.status(200).json({ message: '갸차상품 전체조회', data: allGachaList });
 });
 
-// 가차 시행 API 예시 (웅상)
+// 가챠 수행용 구매 라우터 api ver.1
+router.post('/gacha/buy/:gachaTry', authMiddleware, async (req, res, next) => {
+    try {
+        const { gachaTry } = req.params;
+        const { contains } = req.body;
+        const { accountId } = req.user;
+
+        // 가챠 테이블의 상품명이 포함하는 키워드를 body로 받아 검색
+        const gachaInfo = await prisma.gacha.findFirst({
+            where: {
+                gachaName: {
+                    contains: contains,
+                },
+            },
+        });
+
+        const gachaPrice = gachaInfo.gachaPrice * gachaTry;
+
+        // 선불 요금제
+        if (req.user.cash < +gachaPrice) {
+            throw new Error('notEnoughMoney');
+        }
+
+        //랜덤 선수 로또
+        const gachaTransaction = await prisma.$transaction(
+            async tx => {
+                const resultGacha = await gacha(gachaTry, accountId);
+                const createGacha = resultGacha.map(({ playerName, ...rest }) => rest);
+                const findName = resultGacha.map(({ playerName }) => playerName);
+
+                const cashGo = await prisma.account.update({
+                    data: {
+                        cash: req.user.cash - gachaPrice,
+                    },
+                    where: {
+                        accountId: +accountId,
+                    },
+                });
+                // 위에서 뽑은 결과로 createMany
+                await prisma.roster.createMany({
+                    data: createGacha,
+                });
+
+                return [cashGo, findName];
+            },
+            {
+                isolationLevel: Prisma.TransactionIsolationLevel.ReadCommitted,
+            },
+        );
+
+        return res.status(201).json({ message: `${gachaTransaction[1]} 선수를 획득했습니다. 남은 Cash : ${gachaTransaction[0].cash}` });
+    } catch (error) {
+        switch (error.message) {
+            case 'notEnoughMoney':
+                return res.status(409).json({ message: ' Cash 가 부족합니다.' });
+            default:
+                return res.status(401).json({ message: error.message ?? '비정상적인 요청입니다.' });
+        }
+    }
+});
+
+// 가챠 로직 라우터 ver.2
 router.post('/gacha/buy', authMiddleware, async (req, res, next) => {
     try {
         const { gachaTry, contains } = req.body;
@@ -40,15 +101,14 @@ router.post('/gacha/buy', authMiddleware, async (req, res, next) => {
             },
         });
 
-        if(gachaInfo.gachaPrice * gachaTry > cash)
-            return res.status(400).json({ message : "잔액이 부족합니다." })
+        if (gachaInfo.gachaPrice * gachaTry > cash) return res.status(400).json({ message: '잔액이 부족합니다.' });
 
         // 잔액부터 변동시킴
         const gachaTransaction = await prisma.$transaction(
             async tx => {
                 const updatedCash = await tx.account.update({
                     where: { accountId: +accountId },
-                    data: { cash: cash - (gachaInfo.gachaPrice * gachaTry) },
+                    data: { cash: cash - gachaInfo.gachaPrice * gachaTry },
                 });
                 const purchaseHistory = await tx.purchaseHistory.create({
                     data: {
@@ -58,7 +118,7 @@ router.post('/gacha/buy', authMiddleware, async (req, res, next) => {
                     },
                 });
 
-                return [updatedCash, purchaseHistory];
+                return updatedCash;
             },
             {
                 isolationLevel: Prisma.TransactionIsolationLevel.ReadCommitted,
@@ -71,72 +131,16 @@ router.post('/gacha/buy', authMiddleware, async (req, res, next) => {
         //     data: resultGacha,
         // });
 
-        console.log('gachaTransaction:', gachaTransaction)
-        console.log('resultGacha', resultGacha)
+        console.log('gachaTransaction:', gachaTransaction);
+        console.log('resultGacha', resultGacha);
 
-        return res.status(201).json({ message: `${resultGacha[0].playerName} 선수를 획득했습니다. 남은 Cash : ${gachaTransaction[0].cash}` });
+        return res.status(201).json({
+            message: `${resultGacha[0].playerName} 선수를 획득했습니다. 남은 Cash : ${gachaTransaction[0].cash}`,
+        });
     } catch (error) {
         switch (error.message) {
             case 'notEnoughMoney':
                 return res.status(400).json({ message: ' 소지금이 부족합니다.' });
-            default:
-                return res.status(401).json({ message: error.message ?? '비정상적인 요청입니다.' });
-        }
-    }
-});
-
-// 상품 구매 api 예시 (웅상)
-router.post('/gacha/buy/:gachaTry', authMiddleware, async (req, res, next) => {
-    try {
-        const { gachaTry } = req.params;
-
-        const { accountId } = req.user;
-
-        // // 해당 상품 찾기
-        // const findtry = await prisma.gacha.findFirst({
-        //     where: {
-        //         productId: +productId,
-        //     },
-        // });
-
-        // if (!findtry) {
-        //     return res.status(401).json({ message: ' 없는 상품 번호 입니다. ' });
-        // }
-
-        const gachaPrice = gachaTry * 1000;
-
-        console.log(gachaTry);
-        // 선불 요금제
-        if (req.user.cash < +gachaPrice) {
-            throw new Error('notEnoughMoney');
-        }
-
-        const cashGo = await prisma.account.update({
-            data: {
-                cash: req.user.cash - gachaPrice,
-            },
-            where: {
-                accountId: +accountId,
-            },
-        });
-
-        //랜덤 선수 로또
-        const resultGacha = await gacha(gachaTry, accountId);
-
-        const createGacha = resultGacha.map(({ playerName, ...rest }) => rest);
-        const findName = resultGacha.map(({ playerName }) => playerName);
-        console.log(findName);
-
-        // 위에서 뽑은 결과로 createMany
-        await prisma.roster.createMany({
-            data: createGacha,
-        });
-
-        return res.status(201).json({ message: `${findName} 선수를 획득했습니다. 남은 Cash : ${cashGo.cash}` });
-    } catch (error) {
-        switch (error.message) {
-            case 'notEnoughMoney':
-                return res.status(409).json({ message: ' Cash 가 부족합니다.' });
             default:
                 return res.status(401).json({ message: error.message ?? '비정상적인 요청입니다.' });
         }
